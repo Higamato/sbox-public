320fa9a0292c022a9e649349eee247710de93f7c
Lorenz Junglas <4759511+lolleko@users.noreply.github.com>
2026-03-05 08:24:26 +0100

    Avoid CallbackBatch action allocations (#4201)
    * Avoid callbackbatch aciton allocation and closure
    
    Store the CommonCallback and GameObject/Comonent Instance instead of an action
    
    * Scene push uses struct instead of DisposeAction
    
    Avoids action allocation

--- engine/Sandbox.Engine/Scene/Components/Component.cs
+++ engine/Sandbox.Engine/Scene/Components/Component.cs
@@ -11,6 +11,24 @@ namespace Sandbox;
 public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 {
 	/// <summary>
+	/// Invokes the callback for the given <paramref name="callback"/> type.
+	/// Called internally by <see cref="CallbackBatch"/> to avoid delegate allocations.
+	/// </summary>
+	internal void InvokeCallback( CommonCallback callback )
+	{
+		switch ( callback )
+		{
+			case CommonCallback.Awake: InternalOnAwake(); break;
+			case CommonCallback.Enable: OnEnabledInternal(); break;
+			case CommonCallback.Disable: OnDisabledInternal(); break;
+			case CommonCallback.Destroy: OnDestroyInternal(); break;
+			case CommonCallback.Validate: OnValidateInternal(); break;
+			case CommonCallback.Dirty: OnDirtyInternal(); break;
+			case CommonCallback.Loading: LaunchLoader(); break;
+		}
+	}
+
+	/// <summary>
 	/// The scene this Component is in. This is a shortcut for `GameObject.Scene`.
 	/// </summary>
 	[ActionGraphInclude]
@@ -58,7 +76,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 
 		if ( ShouldExecute )
 		{
-			CallbackBatch.Add( CommonCallback.Awake, InternalOnAwake, this, "OnAwake" );
+			CallbackBatch.Add( CommonCallback.Awake, this, "OnAwake" );
 		}
 	}
 
@@ -278,7 +296,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 
 			if ( ShouldExecute )
 			{
-				CallbackBatch.Add( CommonCallback.Enable, OnEnabledInternal, this, "OnEnabled" );
+				CallbackBatch.Add( CommonCallback.Enable, this, "OnEnabled" );
 			}
 
 			Scene.RegisterComponent( this );
@@ -287,7 +305,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 		{
 			if ( ShouldExecute )
 			{
-				CallbackBatch.Add( CommonCallback.Disable, OnDisabledInternal, this, "OnDisabled" );
+				CallbackBatch.Add( CommonCallback.Disable, this, "OnDisabled" );
 			}
 
 			Scene.UnregisterComponent( this );
@@ -332,7 +350,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 			return;
 
 		GameObject.Components.OnDestroyedInternal( this );
-		CallbackBatch.Add( CommonCallback.Destroy, OnDestroyInternal, this, "OnDestroy" );
+		CallbackBatch.Add( CommonCallback.Destroy, this, "OnDestroy" );
 
 		if ( _enabledState )
 		{
@@ -342,7 +360,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 
 			if ( ShouldExecute )
 			{
-				CallbackBatch.Add( CommonCallback.Disable, OnDisabledInternal, this, "OnDisabled" );
+				CallbackBatch.Add( CommonCallback.Disable, this, "OnDisabled" );
 			}
 		}
 	}
@@ -396,7 +414,7 @@ public abstract partial class Component : IJsonConvert, IComponentLister, IValid
 
 	internal void Validate()
 	{
-		CallbackBatch.Add( CommonCallback.Validate, OnValidateInternal, this, "OnValidate" );
+		CallbackBatch.Add( CommonCallback.Validate, this, "OnValidate" );
 	}
 
 	internal void OnRefreshInternal()
